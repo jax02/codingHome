bd2ebc930a871803af166492dd59215610a4b566
Alvin <jimmyjimmy66666@gmail.com>
2022-08-30 10:05:41 +0800

    page3

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,6 +1,26 @@
-console.log('Hello!');
+// console.log('Hello!');
 
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+// $(document).ready(() => {
+//   console.log('HesSchool Hello!');
+// });
+console.log('test');
+
+// swiper初始化設定
+const swiper = new Swiper('.mySwiper', {
+  // Optional parameters
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  
+  //pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
-
