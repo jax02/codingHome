d99bd7ef567ed8a56fc5bd2be4fabc05096cf700
Alvin <jimmyjimmy66666@gmail.com>
2022-08-30 10:05:18 +0800

    Update 2022-08-30T02:05:15.902Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,25 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
+// console.log('Hello!');
+// $(document).ready(() => {
+//   console.log('HesSchool Hello!');
+// });
+console.log('test'); // swiper初始化設定
+
+var swiper = new Swiper('.mySwiper', {
+  // Optional parameters
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  //pagination
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
 });
 //# sourceMappingURL=all.js.map
